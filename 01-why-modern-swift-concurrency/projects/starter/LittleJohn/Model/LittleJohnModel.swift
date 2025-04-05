97bd39bb79e314988bb36a3c206a08d0b364f344
Jim Mccarty <huckncatch@fastmail.com>
2025-04-04 18:06:36 -0700

    1.7 Using asynchronous sequences - formatting

--- 01-why-modern-swift-concurrency/projects/starter/LittleJohn/Model/LittleJohnModel.swift
+++ 01-why-modern-swift-concurrency/projects/starter/LittleJohn/Model/LittleJohnModel.swift
@@ -37,35 +37,35 @@ extension String: Error { }
 
 /// The app model that communicates with the server.
 class LittleJohnModel: ObservableObject {
-  /// Current live updates.
-  @Published private(set) var tickerSymbols: [Stock] = []
+    /// Current live updates.
+    @Published private(set) var tickerSymbols: [Stock] = []
 
-  /// Start live updates for the provided stock symbols.
-  func startTicker(_ selectedSymbols: [String]) async throws {
-    guard let url = URL(string: "http://localhost:8080/littlejohn/ticker?\(selectedSymbols.joined(separator: ","))") else {
-      throw "The URL could not be created."
-    }
-  }
-
-  /// A URL session that lets requests run indefinitely so we can receive live updates from server.
-  private lazy var liveURLSession: URLSession = {
-    var configuration = URLSessionConfiguration.default
-    configuration.timeoutIntervalForRequest = .infinity
-    return URLSession(configuration: configuration)
-  }()
-
-  ///
-  func availableSymbols() async throws -> [String] {
-    guard let url = URL(string: "http://localhost:8080/littlejohn/symbols")
-    else {
-      throw "The URL could not be created."
+    /// Start live updates for the provided stock symbols.
+    func startTicker(_ selectedSymbols: [String]) async throws {
+        guard let url = URL(string: "http://localhost:8080/littlejohn/ticker?\(selectedSymbols.joined(separator: ","))") else {
+            throw "The URL could not be created."
+        }
     }
 
-    let (data, response) = try await URLSession.shared.data(from: url)
-    guard (response as? HTTPURLResponse)?.statusCode == 200 else {
-      throw "The server responded with an error."
-    }
+    /// A URL session that lets requests run indefinitely so we can receive live updates from server.
+    private lazy var liveURLSession: URLSession = {
+        var configuration = URLSessionConfiguration.default
+        configuration.timeoutIntervalForRequest = .infinity
+        return URLSession(configuration: configuration)
+    }()
 
-    return try JSONDecoder().decode([String].self, from: data)
-  }
+    ///
+    func availableSymbols() async throws -> [String] {
+        guard let url = URL(string: "http://localhost:8080/littlejohn/symbols")
+        else {
+            throw "The URL could not be created."
+        }
+
+        let (data, response) = try await URLSession.shared.data(from: url)
+        guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+            throw "The server responded with an error."
+        }
+
+        return try JSONDecoder().decode([String].self, from: data)
+    }
 }
