3dd5b6f47d08b994bcfb6486eda556ef3042f489
Jim Mccarty <huckncatch@fastmail.com>
2025-04-09 19:24:15 -0700

    1.9 Handling cancellation errors

--- 01-why-modern-swift-concurrency/projects/starter/LittleJohn/Model/LittleJohnModel.swift
+++ 01-why-modern-swift-concurrency/projects/starter/LittleJohn/Model/LittleJohnModel.swift
@@ -58,7 +58,6 @@ class LittleJohnModel: ObservableObject {
 
             await MainActor.run {
                 tickerSymbols = sortedSymbols
-                print("Updated: \(Date())")
             }
         }
     }
